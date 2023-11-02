f513c8cc29a74a3e876ce38afbedec8304ed1507
Akaibek <ganyev.2005@gmail.com>
2023-11-02 15:24:49 +0600

    Implementation of Observer

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Интерфейс для подписчиков (наблюдателей)
 interface Observer {
     void update(String videoTitle);
 }
 
-// Класс для канала (субъекта)
 class YouTubeChannel {
     private final List<Observer> subscribers = new ArrayList<>();
     private String latestVideoTitle;
 
-    // Метод для добавления подписчика
     public void subscribe(Observer subscriber) {
         subscribers.add(subscriber);
     }
 
-    // Метод для удаления подписчика
     public void unsubscribe(Observer subscriber) {
         subscribers.remove(subscriber);
     }
 
-    // Метод для опубликования нового видео
     public void publishNewVideo(String videoTitle) {
         latestVideoTitle = videoTitle;
         notifySubscribers();
     }
 
-    // Метод для уведомления всех подписчиков
     private void notifySubscribers() {
         for (Observer subscriber : subscribers) {
             subscriber.update(latestVideoTitle);
@@ -35,7 +29,6 @@ class YouTubeChannel {
     }
 }
 
-// Класс для
 class Subscriber implements Observer {
     private final String name;
 
@@ -43,7 +36,6 @@ class Subscriber implements Observer {
         this.name = name;
     }
 
-    // Метод, вызываемый при получении уведомления о новом видео
     @Override
     public void update(String videoTitle) {
         System.out.println(name + " получил уведомление о новом видео: " + videoTitle);
@@ -52,19 +44,15 @@ class Subscriber implements Observer {
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем канал и подписчиков
         YouTubeChannel channel = new YouTubeChannel();
         Subscriber subscriber1 = new Subscriber("Подписчик 1");
         Subscriber subscriber2 = new Subscriber("Подписчик 2");
 
-        // Подписываем подписчиков на канал
         channel.subscribe(subscriber1);
         channel.subscribe(subscriber2);
 
-        // Опубликовываем новое
         channel.publishNewVideo("Видео 1");
 
-        // Отписываем одного из подписчиков
         channel.unsubscribe(subscriber1);
 
 
